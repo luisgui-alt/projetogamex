831525db4767c73256a867b34050a19f5e742adf
Luis Guilherme <luiguinm@gmail.com>
2025-06-26 17:30:52 -0300

    dados e imagens

---
+++ script/games.js
@@ -0,0 +1,90 @@
+const availableGames = [
+    {
+        id: 0,
+        nome: "Getea 7",
+        capa: "gta7.jpg",
+        desenvolvedora: "Pedra Estrela Jogos",
+        lancamento: "2034",
+        genero: "ação",
+        preco: "387",
+        classificacao: "+18",
+        descricao: "Última entrada da aclamada franquia Getea, onde você faz o que bem entender e como quiser!! Com uma história alucinante e um mundo aberto de cair o queixo!! UUUUOOOOUUUUU"
+    },
+    {
+        id: 1,
+        nome: "Metal Queer Flacid 5: Pinto Pain",
+        capa: "mqs.jpg",
+        desenvolvedora: "KUnhame",
+        lancamento: "2015",
+        genero: "ação/espionagem",
+        preco: "270",
+        classificacao: "+18",
+        descricao: "Buscando a verdade sobre sua identidade, Big Balls se vê encurralado em inúmeras teorias da conspiração. Cabe você ajudá-lo a descobrir a verdade e revelar tudo ao mundo antes que as coisas saiam do controle."
+    },
+    {
+        id: 2,
+        nome: "Vermelho Morte Redencao 2",
+        capa: "rdr.jpg",
+        desenvolvedora: "Pedra Estrela Jogos",
+        lancamento: "2028",
+        genero: "ação/aventura",
+        preco: "420",
+        classificacao: "+18",
+        descricao: "Curte Bang Bang? Curte Caubóis com tendências homossexuais? Curte diálogos arrastados com muito significado? Então este jogo é pra você!! Gráficos insanos e gameplay fluída para você se sentir no próprio faroeste!!"
+    },
+    {
+        id: 3,
+        nome: "The Lésbion of Zélda: Peitinho of Time",
+        capa: "zelda.jpg",
+        desenvolvedora: "Nintonta",
+        lancamento: "1998 (remake em 2099)",
+        genero: "ação/aventura",
+        preco: "950",
+        classificacao: "+12",
+        descricao: "Apesar da gameplay datada e o gráfico pixelado, o jogo se sustenta até hoje com sua magia inexplicável. Talvez o preço seja um pouco salgado? Talvez."
+    },
+    {
+        id: 4,
+        nome: "FIFA 2077: Ultimate Bug Edition",
+        capa: "fifa.jpg",
+        desenvolvedora: "IE Sucks",
+        lancamento: "2076",
+        genero: "esporte",
+        preco: "699",
+        classificacao: "Livre",
+        descricao: "Agora com 0,001% de inovação em relação à versão anterior, FIFA 2077 traz as mesmas mecânicas, os mesmos bugs e os mesmos comentários repetidos — mas com novos uniformes! Compre agora e desbloqueie o modo 'Cartola Cyberpunk'."
+    },
+    {
+        id: 5,
+        nome: "Menescraft",
+        capa: "menescraft.jpg",
+        desenvolvedora: "Moyang",
+        lancamento: "2050",
+        genero: "sobrevivência/blocos",
+        preco: "187",
+        classificacao: "Livre",
+        descricao: "Deixe a criatividade tomar conta e a crueldade insaciável fluir neste mundo quadrado de Menescraft!! Infinitas possibilidades para o caos e tortura de criaturas inocentes!! Construir castelos?? Isso é coisa de bixa, sevicías alheias divertem qualquer um!!"
+    },
+    {
+        id: 6,
+        nome: "Diddy of Diddy",
+        capa: "call.jpg",
+        desenvolvedora: "AtivarVisão",
+        lancamento: "2026",
+        genero: "tiro",
+        preco: "400",
+        classificacao: "+16",
+        descricao: "Explosões, gritos e gráficos realistas onde você mal vê o inimigo. Assuma o papel de um soldado genérico em mais uma guerra genérica, com DLCs já no disco! Novo modo campanha: dura 12 minutos!"
+    },
+    {
+    id: 7,
+    nome: "Meio da Vida: Capítulo Nunca Final",
+    capa: "half.jpg",
+    desenvolvedora: "Válvula Entupida",
+    lancamento: "???",
+    genero: "tiro/física quântica",
+    preco: "333",
+    classificacao: "+16",
+    descricao: "Você é Gordon Frango, um físico teórico armado com um pé de cabra e um olhar vazio. Após um experimento que deu muito errado (como sempre), você precisa atravessar laboratórios infestados de alienígenas, soldados nervosos e teorias que nem Einstein entenderia. E não se preocupe: o capítulo 3 nunca vem!"
+    }
+]
